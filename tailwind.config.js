3451d602e156bc300cd0bcf665c63b650e7e95a1
summerjalexander <summera@simplytrainingsolutions.com>
2025-11-03 18:41:14 -0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./**/*.{js,ts,jsx,tsx}"  // 👈 this line now includes ALL folders (root, components, hooks, etc.)
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
