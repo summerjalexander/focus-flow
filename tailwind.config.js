5c99cd19f754261a876342a6c3af4bf223713769
summerjalexander <summera@simplytrainingsolutions.com>
2025-11-03 19:02:00 -0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./hooks/**/*.{js,ts,jsx,tsx}",
+    "./services/**/*.{js,ts,jsx,tsx}",
+    "./App.tsx",
   ],
   theme: {
     extend: {},
